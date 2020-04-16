e6a55f62abcfe7655c8ae4706f8558dccf1fb955
charlottenpl <1737980290@qq.com>
2020-04-16 17:11:56 +0800

    4.16统一使用UTF-8

--- src/main/java/com/Config/MvcConfig.java
+++ src/main/java/com/Config/MvcConfig.java
@@ -1,39 +1,52 @@
-package com.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-/**
- * 中文乱码解决
- */
-@Configuration
-@EnableWebMvc
-@ComponentScan("com")
-public class MvcConfig implements WebMvcConfigurer {
-
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter() {
-        StringHttpMessageConverter converter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
-        return converter;
-    }
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(responseBodyConverter());
-
-    }
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorPathExtension(false);
-    }
-
-}
+//package com.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.StringHttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.nio.charset.Charset;
+//import java.nio.charset.StandardCharsets;
+//import java.util.List;
+//
+///**
+// * 中文乱码解决
+// */
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan("com")
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    @Bean
+//    public HttpMessageConverter<String> responseBodyConverter() {
+//        StringHttpMessageConverter converter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+//        return converter;
+//    }
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(responseBodyConverter());
+//
+//    }
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer.favorPathExtension(false);
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations(
+//                "classpath:/static/");
+//        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+//                "classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations(
+//                "classpath:/META-INF/resources/webjars/");
+//        registry.addResourceHandler("/image/**").addResourceLocations(
+//                "classpath:/image");
+//        WebMvcConfigurer.super.addResourceHandlers(registry);
+//    }
+//}
