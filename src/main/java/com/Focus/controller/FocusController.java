bf1b3df0bdefdfb969dd642980dfa2e0c2646231
charlottenpl <1737980290@qq.com>
2020-05-08 19:38:19 +0800

    5.8修改focus传数据类型

--- src/main/java/com/Focus/controller/FocusController.java
+++ src/main/java/com/Focus/controller/FocusController.java
@@ -1,8 +1,12 @@
 package com.Focus.controller;
 
+import com.Doctor.service.DoctorService;
 import com.Focus.service.FocusService;
+import com.Medicine.service.MedicineService;
 import com.entity.Doctor;
 import com.entity.Focus;
+import com.entity.Medicine;
+import com.entity.ToFocus;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import io.swagger.annotations.Api;
@@ -11,8 +15,10 @@ import javafx.geometry.Pos;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.print.Doc;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/focus")
@@ -21,20 +27,56 @@ public class FocusController {
 
     @Resource
     FocusService focusService;
+    @Resource
+    DoctorService doctorService;
+    @Resource
+    MedicineService medicineService;
     Gson gson = new Gson();
 
     //获取收藏医生列表
     @ApiOperation("获取关注医生的列表")
     @RequestMapping(value = "/doctorList",method = RequestMethod.GET)
     public String findDoctor(@RequestParam("userId") int userId,@RequestParam("userType") int userType){
-        return gson.toJson(this.focusService.findDoctorList(userId,userType));
+        List<Focus> focusList =  this.focusService.findDoctorList(userId,userType);
+        List<ToFocus> toFocusList = new ArrayList<>();
+        Doctor doctor = null;
+        for (Focus f : focusList){
+            doctor = doctorService.findById(f.getTypeId());
+            ToFocus toFocus = new ToFocus();
+            toFocus.setId(f.getId());
+            toFocus.setImg(doctor.getHeadImg());
+            toFocus.setMore(doctor.getResume());
+            toFocus.setName(doctor.getName());
+            toFocus.setType(f.getType());
+            toFocus.setTypeId(f.getTypeId());
+            toFocus.setUserId(f.getUserId());
+            toFocus.setUserType(f.getUserType());
+            toFocusList.add(toFocus);
+        }
+        return gson.toJson(toFocusList);
     }
 
     //获取收藏药品列表
     @ApiOperation("获取收藏药品的列表")
         @RequestMapping(value = "/medicineList",method = RequestMethod.GET)
     public String findMedicine(@RequestParam("userId") int userId,@RequestParam("userType") int userType){
-        return gson.toJson(this.focusService.findMedicine(userId,userType));
+        List<Focus> focusList =  this.focusService.findMedicine(userId,userType);
+        List<ToFocus> toFocusList = new ArrayList<>();
+        Medicine medicine = null;
+        for (Focus f : focusList){
+            medicine = medicineService.findById(f.getTypeId());
+            ToFocus toFocus = new ToFocus();
+            toFocus.setId(f.getId());
+            toFocus.setImg(medicine.getImg1());
+            toFocus.setMore(medicine.getMedicine());
+            toFocus.setName(medicine.getGeneralName());
+            toFocus.setType(f.getType());
+            toFocus.setTypeId(f.getTypeId());
+            toFocus.setUserId(f.getUserId());
+            toFocus.setUserType(f.getUserType());
+            toFocusList.add(toFocus);
+        }
+        return gson.toJson(toFocusList);
     }
 
     //取消收藏
